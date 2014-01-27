e370f36049b51858cb0126b26214118d8f6e48e2
Rorlin <sryan@pbp1.com>
2014-01-27 10:12:33 -0500

    Test

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,58 @@
-# $('./body') {
-#   insert_top("header", class: "_header") {
-#     Move stuff here
-#   }
-# }
+$(".//div[@id='Header']") {
+  $("./div[@id='Logo']") {
+    # Move top menu into #Logo container
+    move_here("../../../div[@id='TopMenu']", "bottom") {
+		# Remove unwanted top menu items from upper menu
+		remove(".//li[not(contains(@class, 'CartLink') or contains(@class, 'First'))]")
+	}
+  }
+	# Insert HTML for bottom of header
+	insert("div", class: "mw_header_bottom") {
+	insert("div", class: "mw_search")
+	insert("div", class: "mw_menu_btn sprites-menu", data-ur-toggler-component: "button")
+	}
+}
+$("..//div[@class='mw_search']"){
+
+	move_here("//div[@id='SearchForm']","bottom"){
+	
+		remove("./p")
+	
+		$("./form") {
+		
+			remove("./label")
+			
+			$("./input[@type='text']"){
+			
+				attribute("placeholder", "Search...")
+			
+			}
+			
+			$("./input[@type='image']"){
+			
+				wrap("div", class: "mw_search_btn sprites-search")
+			
+			}
+		
+		}
+	
+	}
+	
+}
+
+# Move menu items into header
+move_here("../div[@id='Menu']", "bottom") {
+
+	attribute("data-ur-toggler-component", "content")
+	# Move "Category List" element on bottom of #Menu element
+	move_here("../../div[@id='Wrapper']/div[@class='Left']/div[@id='SideCategoryList']", "bottom")
+	
+	$$("li") {
+	
+		add_class("mw_bar2")
+		
+	}
+}
+
+
+
